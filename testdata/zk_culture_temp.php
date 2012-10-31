63edfa9e5d2814288a8c2dc7092e9ff6569c194c
zk19802133 <zk19802133@gmail.com>
2012-10-31 20:07:05 +0800

    some page temp

---
+++ testdata/zk_culture_temp.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>
+            [!--pagetitle--]
+        </title>
+        <meta name="keywords" content="[!--pagekey--]">
+        <meta name="description" content="[!--pagedes--]">
+        <link rel="stylesheet" href="[!--news.url--]skin/default/css/yatax.css" type="text/css" media="screen, project, print">
+        <link rel="stylesheet" href="[!--news.url--]skin/default/css/globle_cn_ie6.css" type="text/css" media="screen, project, print">
+        <link rel="stylesheet" href="[!--news.url--]skin/default/css/jquery.ui.theme.css" type="text/css" media="screen, project, print">
+        <script type="text/javascript" src="[!--news.url--]skin/default/js/jquery-1.8.2.js">
+</script>
+    </head>
+    <body>
+        [!--temp.header--] 
+        <div id="container">
+            <div class="bound body">
+                <div>
+                    您当前的位置：[!--newsnav--]
+                </div>
+                <div class="grid215 left">
+                    <div class='leftNav' id='1001'>
+                        <div class='top' id='1002'></div>
+                        <div class='content' id='1003'>
+                        [showclasstemp]64,13,0,0[/showclasstemp]
+                        </div>
+                        <div class='bottom' id='1004'></div>
+                    </div>
+                </div>
+                <div class="grid765_r left">
+                    <!--HWDocContent-->
+                    <div class="core-network">
+                        <div class="content">
+                                <div class="title">
+                                    公司理念
+                                </div>
+                                <p>
+                                    <img src="/skin/default/images/about1.jpg">
+                                </p>
+                                <div class="detail">
+                                    <p>
+                                        客户：<br>
+                                        客户是我们永远的伙伴和最宝贵的财富。 我们尊重客户，理解客户，并致力于持续提供超越客户期望的产品和服务，不懈提升我们的技术、经验、创新力和应变能力，即时对客户的各种需求做出反应。客户满意，是我们的最高宗旨。
+                                    </p>
+                                    <p>
+                                        员工理念：<br>
+                                        1．品德修养是进入我们团队的先决条件；<br>
+                                        2．惟有贡献证明个体的价值；<br>
+                                        3．永远的求变精神，昨天的经验也许会成为今天的障碍；<br>
+                                        4．执行力就是在思考之后做好每一个细节；<br>
+                                        5．工作是享受人生的重要方式。
+                                    </p>
+                                    <p>
+                                        公司理念：<br>
+                                        独立、诚信、创新、共赢、社会责任
+                                    </p>
+                            </div>
+                                </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>[!--temp.footer--]
+    </body>
+</html>
---
+++ testdata/zk_team_temp.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>
+            [!--pagetitle--]
+        </title>
+        <meta name="keywords" content="[!--pagekey--]">
+        <meta name="description" content="[!--pagedes--]">
+        <link rel="stylesheet" href="[!--news.url--]skin/default/css/yatax.css" type="text/css" media="screen, project, print">
+        <link rel="stylesheet" href="[!--news.url--]skin/default/css/globle_cn_ie6.css" type="text/css" media="screen, project, print">
+        <link rel="stylesheet" href="[!--news.url--]skin/default/css/jquery.ui.theme.css" type="text/css" media="screen, project, print">
+        <script type="text/javascript" src="[!--news.url--]skin/default/js/jquery-1.8.2.js">
+</script>
+    </head>
+    <body>
+        [!--temp.header--]
+        <div id="container">
+            <div class="bound body">
+                <div>
+                    您当前的位置：[!--newsnav--]
+                </div>
+                <div class="grid215 left">
+                    <div class='leftNav' id='1001'>
+                        <div class='top' id='1002'></div>
+                        <div class='content' id='1003'>
+                            [showclasstemp]64,13,0,0[/showclasstemp]
+                        </div>
+                        <div class='bottom' id='1004'></div>
+                    </div>
+                </div>
+                <div class="grid765_r left">
+                    <!--HWDocContent-->
+                    <div class="core-network">
+                        <div class="content">
+                            <div class="title">
+                                专家团队
+                            </div>
+                            <div class="detail">
+                                <div class="hr"></div>
+                                <div class="teamintro">
+                                    <div class="teampic">
+                                        <img src="/skin/default/images/aihua.jpg">
+                                    </div>
+                                    <div>
+                                        艾华 男 1959年生，中南财经政法大学教授、经济学博士、财政税收专业硕士生、博士生导师、财政税务学院税务系主任，中南财经政法大学《税法》课程首席教师。兼任中国注册税务师协会、中国税收筹划研究会副会长、武汉市注册税务师协会副会长，财政部全国注册会计师考试委员会、国家税务总局全国注册税务师考试管理中心、中国税收咨询网特聘专家。
+                                    </div>
+                                </div>
+                                <div class="hr"></div>
+                                <div class="teamintro">
+                                    <div class="teampic">
+                                        <img src="/skin/default/images/aihua.jpg">
+                                    </div>
+                                    <div>
+                                        艾华 男 1959年生，中南财经政法大学教授、经济学博士、财政税收专业硕士生、博士生导师、财政税务学院税务系主任，中南财经政法大学《税法》课程首席教师。兼任中国注册税务师协会、中国税收筹划研究会副会长、武汉市注册税务师协会副会长，财政部全国注册会计师考试委员会、国家税务总局全国注册税务师考试管理中心、中国税收咨询网特聘专家。
+                                    </div>
+                                </div>
+                                <div class="hr"></div>
+                                <div class="teamintro">
+                                    <div class="teampic">
+                                        <img src="/skin/default/images/aihua.jpg">
+                                    </div>
+                                    <div>
+                                        艾华 男 1959年生，中南财经政法大学教授、经济学博士、财政税收专业硕士生、博士生导师、财政税务学院税务系主任，中南财经政法大学《税法》课程首席教师。兼任中国注册税务师协会、中国税收筹划研究会副会长、武汉市注册税务师协会副会长，财政部全国注册会计师考试委员会、国家税务总局全国注册税务师考试管理中心、中国税收咨询网特聘专家。
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>[!--temp.footer--]
+    </body>
+</html>
