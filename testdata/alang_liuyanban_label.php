129c384627f9bf9136c8eebc1a772c3de6fee30b
anuxs <alang.yl@gmail.com>
2012-12-10 11:56:59 +0800

    add liuyanban

---
+++ testdata/alang_liuyanban_label.php
@@ -0,0 +1,14 @@
+//页面模板内容(*)
+[!--empirenews.listtemp--]<!--list.var1-->[!--empirenews.listtemp--]
+
+//列表内容模板(list.var) (*)
+<li>
+留言ID：[!--lyid--]</br>
+留言内容：[!--lytext--]</br>
+回复内容：[!--retext--]</br>
+留言时间：[!--lytime--]</br>
+留言者：[!--name--]</br>
+留言者邮箱：[!--email--]</br>
+序号：[!--no--]</br>
+<hr>
+</li>
